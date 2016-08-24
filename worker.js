20e436915f1be031ae1df74bd330e55018dcdf1d
Suraj Pindoria <suraj.pindoria@yahoo.com>
2016-08-24 15:06:09 -0700

    Initial commit

---
+++ worker.js
@@ -0,0 +1,21 @@
+const ipc = require('electron').ipcRenderer;
+const BrowserWindow = require('electron').remote.BrowserWindow;
+
+let callerWindow;
+
+ipc.on('compute-primes', function(event, fromWindowId) {
+    const fromWindow = BrowserWindow.fromId(fromWindowId);
+    console.log('*** worker.js');
+
+    let n = 1;
+    while (true) {
+        n += 1;
+        for (let i = 2; i <= Math.sqrt(n); i += 1) {
+            if (n % i === 0) {
+                break;
+            }
+        }
+        // found a prime!
+        console.log(n);
+    }
+});
